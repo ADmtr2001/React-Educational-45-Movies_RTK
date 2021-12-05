d8c1baf653ce58e3213af5d1e6b77917b97dd88a
Arsen Dmitrenko <arsen.dmitrenko1994@gmail.com>
2021-12-05 18:45:00 +0200

    add data fetching. add basic css styles. add data rendering.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import { Route, Routes } from "react-router";
+import { BrowserRouter } from "react-router-dom";
 import "./App.scss";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import MovieDetail from "./components/MovieDetail/MovieDetail";
+import PageNotFound from "./components/PageNotFound/PageNotFound";
 
 function App() {
-  return <div className='App'>App</div>;
+  return (
+    <div className='App'>
+      <BrowserRouter>
+        <Header />
+        <div className='container'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/movie/:imdbID' element={<MovieDetail />} />
+            <Route path='*' element={<PageNotFound />} />
+          </Routes>
+        </div>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
